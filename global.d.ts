39cc4a556eba126055a5b34c1bd44a1e312f820a
Jyolsna Maria Joemon <121849188+jyolsnajoemon@users.noreply.github.com>
2024-01-29 21:39:48 +0530

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,3 @@
-'use-sound' {
-  export default function useSound(sound: any, options?: any): any;
-}
+declare module 'use-sound' {
+    export default function useSound(sound: any, options?: any): any;
+  }
